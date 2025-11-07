a49b76fe5027706445c2fb1a272680d6ae7431c8
apple <apple@LutfiaUbwa.local>
2025-11-07 14:50:49 +0300

    CartPage UI is Done

--- src/Components/Shop/CartInner.tsx
+++ src/Components/Shop/CartInner.tsx
@@ -23,14 +23,18 @@ function CartInner() {
     return (
         <div className="th-cart-wrapper space-top space-extra-bottom">
             <div className="container">
-                <div className="woocommerce-notices-wrapper">
-                    <div className="woocommerce-message">Shipping costs updated.</div>
-                </div>
-                <form action="#" className="woocommerce-cart-form">
+                
+                
+                <div className="row justify-content-end">
+                    <div className="col-md-8">
+                        {/* <h2 className="h4 summary-title">Cart Items</h2> */}
+                        <div className="woocommerce-message">Cart Items</div>
+                        
+                         <form action="#" className="woocommerce-cart-form">
                     <table className="cart_table">
                         <thead>
                             <tr>
-                                <th>Image</th>
+                                {/* <th>Image</th> */}
                                 <th>Product Name</th>
                                 <th>Price</th>
                                 <th>Quantity</th>
@@ -41,11 +45,11 @@ function CartInner() {
                         <tbody>
                             {cartItems.map(item => (
                                 <tr className="cart_item" key={item.id}>
-                                    <td>
+                                    {/* <td>
                                         <Link className="cart-productimage" to={`/shop/${item.id}`}>
                                             <img width={91} height={91} src={item.image} alt={item.name} />
                                         </Link>
-                                    </td>
+                                    </td> */}
                                     <td>
                                         <Link className="cart-productname" to={`/shop/${item.id}`}>
                                             {item.name}
@@ -75,7 +79,7 @@ function CartInner() {
                                     </td>
                                 </tr>
                             ))}
-                            <tr>
+                            {/* <tr>
                                 <td colSpan={6} className="actions">
                                     <div className="th-cart-coupon">
                                         <input type="text" className="form-control" placeholder="Coupon Code..." />
@@ -84,38 +88,86 @@ function CartInner() {
                                     <button type="submit" className="th-btn">Update cart</button>
                                     <Link to="/shop" className="th-btn">Continue Shopping</Link>
                                 </td>
-                            </tr>
+                            </tr> */}
                         </tbody>
                     </table>
-                </form>
-                <div className="row justify-content-end">
-                    <div className="col-md-8 col-lg-7 col-xl-6">
-                        <h2 className="h4 summary-title">Cart Totals</h2>
+                </form> 
+                        
+                      
+                    </div>
+                    <div className='col-md-4'>
+                    <div className="col-md-8 mb-4">
+                        {/* <h2 className="h4 summary-title">Cart Totals</h2> */}
+                        <div className="woocommerce-message">Booking Information</div>
+                        
                         <table className="cart_totals">
                             <tbody>
                                 <tr>
-                                    <td>Cart Subtotal</td>
+                                    <td>Number of Tickets :</td>
                                     <td>
-                                        <span className="amount"><bdi><span>$</span>{cartTotal}</bdi></span>
+                                        {/* <span className="amount"><bdi><span>$</span>{cartTotal}</bdi></span> */} 1
                                     </td>
                                 </tr>
                             </tbody>
                             <tfoot>
                                 <tr className="order-total">
-                                    <td>Order Total</td>
+                                    <td>Total Amount</td>
                                     <td>
                                         <strong>
-                                            <span className="amount"><bdi><span>$</span>{cartTotal}</bdi></span>
+                                            {/* <span className="amount"><bdi><span>$</span>{cartTotal}</bdi></span> */}2000 TZS
                                         </strong>
                                     </td>
                                 </tr>
                             </tfoot>
                         </table>
                         <div className="wc-proceed-to-checkout mb-30">
-                            <Link to="/checkout" className="th-btn">Proceed to checkout</Link>
+                            <select name="" id="">
+                                <option value="TZS">TZS</option>
+                                <option value="USD">USD</option>
+                            </select>
+                            {/* <Link to="/checkout" className="th-btn">Proceed to checkout</Link> */}
                         </div>
                     </div>
+                    <div className="col-md-8">
+                        <div className="woocommerce-message">Customer Information</div>
+
+                        <div className="billing-fields">
+                            <p className="form-row form-row-first">
+                                {/* <label>Full Name <span className="required">*</span></label> */}
+                                <input type="text" className="input-text"  placeholder='Fullname'/>
+                            </p>
+                            <p className="form-row form-row-last">
+                                {/* <label>Email Address <span className="required">*</span></label> */}
+                                <input type="email" className="input-text" placeholder='Email Address' />
+                            </p>
+                            <div className="clear" />
+                            <p className="form-row form-row-first">
+                                {/* <label>Phone Number <span className="required">*</span></label> */}
+                                <input type="text" className="input-text"  placeholder='Phonenumber'/>
+                            </p>
+                         
+                            
+                            <div className="d-flex justify-content-between gap-3 mb-30">
+                                <div className="wc-proceed-to-checkout">
+                                    <Link to="/checkout" className="th-btn">Checkout</Link>
+                                </div>
+                                <div className="wc-clear">
+                                    <Link to="/shop" className="th-btn">Clear</Link>
+                                </div>
+                            </div>
+
+                           
+
+                        </div>
+
+
+                       
+                       
+                    </div>
+
+                    </div>
                 </div>
+               
             </div>
         </div>
     );
